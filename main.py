e51034f8443e0877942a262b0ec21091d6071a78
Nemo <me@captnemo.in>
2023-12-07 14:53:52 +0530

    Update to 2023 dataset instead

--- main.py
+++ main.py
@@ -6,17 +6,17 @@ def create_table(cursor):
     Create a table in the SQLite database to store ward information.
     """
     cursor.execute('''
-        CREATE VIRTUAL TABLE wards USING geopoly(No,Name)
+        CREATE VIRTUAL TABLE wards USING geopoly(id INT,name,assembly_constituency_id INT,assembly_constituency_name,parliamentary_constituency_id INT,parliamentary_constituency_name)
     ''')
 
-def insert_ward(cursor, _shape, KGISWardNo, KGISWardName):
+def insert_ward(cursor, _shape,id_, name,assembly_constituency_id,assembly_constituency_name,parliamentary_constituency_id,parliamentary_constituency_name):
     """
     Insert ward information into the SQLite database.
     """
     cursor.execute('''
-        INSERT INTO wards (_shape, No, Name)
-        VALUES (?, ?, ?)
-    ''', (_shape, KGISWardNo, KGISWardName))
+        INSERT INTO wards (_shape,id, name,assembly_constituency_id,assembly_constituency_name,parliamentary_constituency_id,parliamentary_constituency_name)
+        VALUES (?, ?, ?,?,?,?,?)
+    ''', (_shape, id_, name,assembly_constituency_id,assembly_constituency_name,parliamentary_constituency_id,parliamentary_constituency_name))
 
 def process_geojson_file(file_path, database_path):
     """
@@ -35,15 +35,25 @@ def process_geojson_file(file_path, database_path):
 
         # Iterate through each feature
         for feature in data['features']:
-            # Extract KGISWardNo and KGISWardName
-            KGISWardNo = feature['properties']['KGISWardNo']
-            KGISWardName = feature['properties']['KGISWardName']
 
             # Iterate through each geometry polygon
             for geometry in feature['geometry']['coordinates']:
-
-                # Insert data into the SQLite database
-                insert_ward(cursor, json.dumps(geometry), KGISWardNo, KGISWardName)
+                for g in geometry:
+                    try:
+                        insert_ward(cursor, 
+                            json.dumps(g), 
+                            feature['properties']['id'],
+                            feature['properties']['name_en'],
+                            feature['properties']['assembly_constituency_id'],
+                            feature['properties']['assembly_constituency_name_en'].split('-', 1)[-1].strip(),
+                            feature['properties']['parliamentary_constituency_id'],
+                            feature['properties']['parliamentary_constituency_name_en']
+                        )
+                        print("Inserted "+feature['properties']['name_en'])
+                    except Exception as e:
+                        print("Failed to insert "+feature['properties']['name_en'])
+                        raise e
+                
 
     # Commit and close the connection
     connection.commit()
@@ -51,7 +61,7 @@ def process_geojson_file(file_path, database_path):
 
 if __name__ == "__main__":
     # Specify the path to the GeoJSON file and SQLite database
-    geojson_file_path = "BBMP.geojson"
+    geojson_file_path = "bbmp-2023.geojson"
     database_path = "bbmp.db"
 
     # Process the GeoJSON file and insert data into SQLite database
